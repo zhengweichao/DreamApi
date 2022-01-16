0eb99cc21199b83dabaa388d11ed6ec192ed9923
vchao <code@vchao.top>
2022-01-16 10:35:23 +0800

    [+] init commit

---
+++ src/main/java/top/vchao/dream/DreamApiApplication.java
@@ -0,0 +1,13 @@
+package top.vchao.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/vchao/dream/DreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.vchao.dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
