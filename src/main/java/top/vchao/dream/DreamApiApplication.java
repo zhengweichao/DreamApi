682435c1427d99f2b220ad97359b975d17f0b5d8
vchao <code@vchao.top>
2022-01-22 13:59:33 +0800

    [+] libs: Swagger依赖

--- src/main/java/top/vchao/dream/DreamApiApplication.java
+++ src/main/java/top/vchao/dream/DreamApiApplication.java
@@ -2,8 +2,10 @@ package top.vchao.dream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
+@EnableOpenApi
 public class DreamApiApplication {
 
     public static void main(String[] args) {
