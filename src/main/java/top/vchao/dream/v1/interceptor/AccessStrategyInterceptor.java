83b641fd2addc1ddbba3a4a9d69680b3b6738e3d
vchao <code@vchao.top>
2022-03-19 00:03:16 +0800

    [+] func: 实现访问策略控制(黑白名单)

--- src/main/java/top/vchao/dream/v1/interceptor/AccessStrategyInterceptor.java
+++ src/main/java/top/vchao/dream/v1/interceptor/AccessStrategyInterceptor.java
@@ -8,18 +8,24 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import top.vchao.dream.v1.config.DreamApiConfig;
 import top.vchao.dream.v1.exception.BlackIpException;
+import top.vchao.dream.v1.exception.NoWhiteIpException;
 import top.vchao.dream.v1.util.IpUtil;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * 黑名单拦截器
+ * IP访问策略拦截器 （黑/白名单）
  */
 @Order(0)
 @Aspect
 @Component
-public class BlackIpInterceptor {
+public class AccessStrategyInterceptor {
+
+    @Resource
+    DreamApiConfig dreamApiConfig;
 
     /**
      * 定义拦截器规则
@@ -33,16 +39,27 @@ public class BlackIpInterceptor {
      */
     @Around(value = "pointCut()")
     public Object around(ProceedingJoinPoint point) throws Throwable {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        //判断是否为黑名单用户
-        String ip = IpUtil.getIpAddress(request);
-        if (isBlackIp(ip)) {
-            // 黑名单，则直接抛出异常
-            throw new BlackIpException();
-        } else {
-            // 不是黑名单，走接口返回
+        // 未设置IP访问策略 -> 则不做处理
+        if (dreamApiConfig.isNoneStrategy()) {
             return point.proceed();
         }
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String ip = IpUtil.getIpAddress(request);
+
+        // 黑名单策略 -> 禁止黑名单访问
+        if (dreamApiConfig.isBlackStrategy()) {
+            if (isBlackIp(ip)) {
+                // 黑名单，则直接抛出异常
+                throw new BlackIpException();
+            }
+        } else {
+            // 白名单策略 -> 仅限白名单访问
+            if (!isWhiteIp(ip)) {
+                // 不在白名单，则直接抛出异常
+                throw new NoWhiteIpException();
+            }
+        }
+        return point.proceed();
     }
 
 
@@ -52,6 +69,14 @@ public class BlackIpInterceptor {
     public boolean isBlackIp(String ip) {
         //  TODO 此处仅用作测试功能，后续会将黑名单 ip 维护在 redis 中
         return "192.168.1.195".equals(ip);
+    }
+
+    /**
+     * 查看 IP 是否在白名单中
+     */
+    public boolean isWhiteIp(String ip) {
+        //  TODO 此处仅用作测试功能
+        return "127.0.0.1".equals(ip);
     }
 
 }
