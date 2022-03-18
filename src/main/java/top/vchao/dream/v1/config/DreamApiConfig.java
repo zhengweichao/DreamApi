3eca1afdc088b229523652ff86431051852878d5
vchao <code@vchao.top>
2022-03-18 23:57:43 +0800

    [+] conf: 增加访问策略属性配置(未实现)

--- src/main/java/top/vchao/dream/v1/config/DreamApiConfig.java
+++ src/main/java/top/vchao/dream/v1/config/DreamApiConfig.java
@@ -1,14 +1,30 @@
 package top.vchao.dream.v1.config;
 
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Configuration
-@Getter
+@Data
+@ConfigurationProperties(prefix = "dream-api")
 public class DreamApiConfig {
 
-    @Value("${dream-api.show-hello:true}")
+    @Value("${show-hello:true}")
     private boolean showHello;
 
+    @Value("${access-strategy:none}")
+    private String accessStrategy;
+
+    public boolean isBlackStrategy() {
+        return "black".equals(accessStrategy);
+    }
+
+    public boolean isWhiteStrategy() {
+        return "white".equals(accessStrategy);
+    }
+
+    public boolean isNoneStrategy() {
+        return !isBlackStrategy() && !isWhiteStrategy();
+    }
 }
