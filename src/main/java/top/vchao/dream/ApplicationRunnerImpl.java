6edec81a13a1b206ee88e18e5ffdf14743c8dfb1
vchao <code@vchao.top>
2022-03-19 13:25:46 +0800

    [+] logs: 项目启动后输出访问策略配置

--- src/main/java/top/vchao/dream/ApplicationRunnerImpl.java
+++ src/main/java/top/vchao/dream/ApplicationRunnerImpl.java
@@ -21,6 +21,16 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
         System.out.println("       Dream Api  start successful ! ");
         System.out.println("       验证接口：http://localhost:8080/v1/hello");
         System.out.println("       接口文档：http://localhost:8080/swagger-ui/index.html");
+
+        // 如果设置了白名单访问策略，则输出白名单信息
+        if (dreamApiConfig.isWhiteStrategy()) {
+            System.out.println("       访问策略：白名单 - " + dreamApiConfig.getWhiteList().toString());
+        } else if (dreamApiConfig.isBlackStrategy()) {
+            System.out.println("       访问策略：黑名单");
+        } else {
+            System.out.println("       访问策略：未设置");
+        }
+
         System.out.println("==================================================================");
 
         // 如果设置不显示 hello 接口，则进行关闭
