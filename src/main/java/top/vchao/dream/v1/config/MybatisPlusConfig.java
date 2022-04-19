7c385234e30eaef9856a14f5e9cc9d7b9b8f8894
vchao <code@vchao.top>
2022-04-19 22:40:20 +0800

    [#] conf: 取消MapperScan配置,采用注解配置

--- src/main/java/top/vchao/dream/v1/config/MybatisPlusConfig.java
+++ src/main/java/top/vchao/dream/v1/config/MybatisPlusConfig.java
@@ -3,13 +3,11 @@ package top.vchao.dream.v1.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@MapperScan("top.vchao.dream.v1.*.mapper*")
 public class MybatisPlusConfig {
 
     @Bean
